427f73c91aa3fe07b7fc88d7e16514161813770a
onyoo <roberto9m9@gmail.com>
2016-03-24 12:20:01 +0100

    Done.

--- js/app/directives/makeLower.js
+++ js/app/directives/makeLower.js
@@ -1,5 +1,20 @@
 function makeLower() {
-	
+	return {
+		// directive can only be used as attribute, not element
+		restrict: 'A',
+		// attribute points to ngModel
+		require: 'ngModel',
+		link: function (scope, element, attrs, ngModel) {
+// must use $parsers not formatter b/c need 2 alter data going into model
+// formatter will not affect new input
+			ngModel.$formatters.push(function(string) {
+				return string.toUpperCase();
+			})
+			ngModel.$parsers.push(function(string) {
+				return string.toLowerCase();
+			});
+		}
+	}
 }
 
 angular
