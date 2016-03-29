6d42d71576a2dccfb9a20e523e26c29f23d9ec7f
frankNowinski <nowinski.frank@gmail.com>
2016-03-28 23:17:55 -0400

    Done.

--- spec/Index.spec.js
+++ spec/Index.spec.js
@@ -1,27 +1,27 @@
 var path = require('path');
 
 describe('Form Test', function() {
-	browser.get('http://localhost:8080');
+  browser.get('http://localhost:8080');
 
-	var coupon = element(by.css('input[name="coupon"]'));
+  var coupon = element(by.css('input[name="coupon"]'));
 
-	describe('Coupon', function () {
-		var messages= element(by.css('input[name="coupon"] + div'));
+  describe('Coupon', function () {
+    var messages= element(by.css('input[name="coupon"] + div'));
 
-		it('should make sure the coupon is incorrect', function() {
-			coupon.click();
-			coupon.sendKeys('21');
-			expect(messages.getText()).toContain('incorrect');
-		});
+    it('should make sure the coupon is incorrect', function() {
+      coupon.click();
+      coupon.sendKeys('21');
+      expect(messages.getText()).toContain('incorrect');
+    });
 
-		it('should make sure the coupon is incorrect', function() {
-			coupon.click();
-			coupon.sendKeys('DDDD21');
-			expect(messages.getText()).toEqual('');
-		});
+    it('should make sure the coupon is incorrect', function() {
+      coupon.click();
+      coupon.sendKeys('DDDD21');
+      expect(messages.getText()).toEqual('');
+    });
 
-		it('should set the coupon to always be lowercase', function() {
-			expect(element(by.css('.coupon')).getText()).toEqual('21dddd21');
-		});
-	});
+    it('should set the coupon to always be lowercase', function() {
+      expect(element(by.css('.coupon')).getText()).toEqual('21dddd21');
+    });
+  });
 });
