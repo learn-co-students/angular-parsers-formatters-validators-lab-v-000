1d3144e7b53525e23ee345233bd462e68d0a8e31
shmulim <shmuel.markel@gmail.com>
2016-03-31 00:35:11 -0700

    Done.

--- js/app/directives/couponCode.js
+++ js/app/directives/couponCode.js
@@ -1,7 +1,16 @@
+angular
+  .module('app')
+  .directive('couponCode', couponCode);
+
 function couponCode() {
+  return {
+    restrict: 'A',
+    require: 'ngModel',
+    link: function($scope, $element, $attributes, $controller){
+      $controller.$validators.coupon = function(value){
+          return /\d{2}[a-z]{4}\d{2}/i.test(value);
+      }
+    }
+  }
 
 }
-
-angular
-	.module('app')
-	.directive('couponCode', couponCode);
