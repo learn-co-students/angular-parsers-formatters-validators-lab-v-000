61fd244ac12e6ac97e1192cdfd426efa2ebae248
pajamaw <pjpeterww@gmail.com>
2016-04-08 13:07:57 -0400

    Done.

--- js/app/directives/makeLower.js
+++ js/app/directives/makeLower.js
@@ -1,5 +1,16 @@
 function makeLower() {
-	
+	return {
+    restrict: 'A',
+    require: 'ngModel',
+    link: function (scope, element, attrs, ngModel){
+      ngModel.$parsers.push(function (string){
+        return string.toLowerCase();
+      });
+     ngModel.$formatters.push(function (string){
+        return string.toLowerCase(); 
+      });
+    }
+  }
 }
 
 angular
