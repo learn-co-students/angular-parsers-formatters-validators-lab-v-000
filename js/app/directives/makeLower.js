5a87279b3b674dd86630cfa8636bb26a48100624
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-04-11 10:49:48 -0400

    Done

--- js/app/directives/makeLower.js
+++ js/app/directives/makeLower.js
@@ -1,14 +1,18 @@
-function MakeLower() {
+function makeLower() {
 	return {
+		restrict: 'A',
 		require: 'ngModel',
 		link: function (scope, element, attrs, ngModel) {
-			ngModel.$formatters.push(function(str){
+			ngModel.$formatters.push(function (str) {
+				return str.toUpperCase();
+			});
+			ngModel.$parsers.push(function (str) {
 				return str.toLowerCase();
-			})
+			});
 		}
 	}
 }
 
 angular
 	.module('app')
-	.directive('makeLower', MakeLower);
+	.directive('makeLower', makeLower);
