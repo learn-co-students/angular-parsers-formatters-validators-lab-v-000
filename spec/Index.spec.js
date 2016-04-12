5d15a768f52eff6bbc02e7373783d0374efdcd4e
Adam King <adam.king0126@gmail.com>
2016-04-12 17:39:35 -0500

    Done.

--- spec/Index.spec.js
+++ spec/Index.spec.js
@@ -6,7 +6,7 @@ describe('Form Test', function() {
 	var coupon = element(by.css('input[name="coupon"]'));
 
 	describe('Coupon', function () {
-		var messages= element(by.css('input[name="coupon"]'));
+		var messages= element(by.css('input[name="coupon"] + div'));
 
 		it('should make sure the coupon is incorrect', function() {
 			coupon.click();
