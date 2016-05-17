d353ab310d4a92fadf81ecf6f493931731aaaaa3
febbraiod <donedesign1@gmail.com>
2016-05-17 17:52:18 -0400

    Done.

--- js/app/directives/couponCode.js
+++ js/app/directives/couponCode.js
@@ -1,5 +1,18 @@
 function couponCode() {
+  return {
+    require: 'ngModel',
+    link: function(scope, elem, attrs, ngModel){
 
+      ngModel.$parsers.push(function(coupon){
+        return coupon.toLowerCase();
+      });
+
+      ngModel.$validators.coupon = function (value) {
+        return /\d{2}[a-z]{4}\d{2}/i.test(value);
+      };
+
+    }
+  };
 }
 
 angular
