45741fa55025ecdcf52a74ee444cd632cf7e68f7
c1505 <coreym247@gmail.com>
2016-05-18 16:43:37 -0400

    work in progress

--- js/app/directives/makeLower.js
+++ js/app/directives/makeLower.js
@@ -1,5 +1,16 @@
 function makeLower() {
-	
+	return {
+    restrict: 'A',
+    require: 'ngModel',
+    link: function (scope, element, attrs, ngModel) {
+      ngModel.$formatters.push(function(str) {
+        return str.toUpperCase();
+      });
+      ngModel.$parsers.push(function(str) {
+        return str.toLowerCase();
+      });
+    }
+  };
 }
 
 angular
