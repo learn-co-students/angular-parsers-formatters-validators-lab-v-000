86c2d3ea00630d90b7f4e61a245d5bc378c16c1e
jsgoldb <jgoldb@yahoo.com>
2016-06-04 13:04:26 -0500

    Done.

--- js/app/directives/couponCode.js
+++ js/app/directives/couponCode.js
@@ -1,5 +1,14 @@
 function couponCode() {
-
+  return {
+    restrict: 'A',
+    require: 'ngModel',
+    link: function(scope, element, attrs, ngModel) {
+      ngModel.$validators.code = function(value) {
+        var patt = /\d{2}[a-z]{4}\d{2}/i;
+        return patt.test(value);
+      };
+    }
+  }
 }
 
 angular
