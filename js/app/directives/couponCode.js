bb9f69fecc54c598d97a39e0234cfd6bd59432df
Daniel Brad <danieltbrad@gmail.com>
2016-07-31 16:37:30 -0700

    fixed component form validation

--- js/app/directives/couponCode.js
+++ js/app/directives/couponCode.js
@@ -3,7 +3,8 @@ function couponCode() {
 		restrict: 'A',
 		require: 'ngModel',
 		link: function (scope, element, attrs, ngModel) {
-			ngModel.$validators.coupon = function (value) {
+			ngModel.$validators.coupon = function (value, scope) {
+				debugger;
 				return /\d{2}[a-z]{4}\d{2}/i.test(value);
 			};
 		}
