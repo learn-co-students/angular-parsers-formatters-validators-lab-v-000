347c2d6d5fd73dd7ff188958788cffb7bb3e5b98
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-08-15 19:16:19 -0400

    Done.

--- js/app/directives/couponCode.js
+++ js/app/directives/couponCode.js
@@ -1,5 +1,14 @@
 function couponCode() {
-
+	return {
+		restrict: 'A',
+		require: 'ngModel',
+		link: function(scope, elem, attrs, ngModel){
+			ngModel.$validators.coupon = function(str){
+				var regex = (/\d{2}[a-z]{4}\d{2}/i);
+				return regex.test(str);
+			};
+		}
+	};
 }
 
 angular
