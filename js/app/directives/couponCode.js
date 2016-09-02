22dc0330b5817bb4d7f24e1079b152642597e4e6
corya0687 <corya0687@gmail.com>
2016-09-02 12:56:19 -0400

    Done.

--- js/app/directives/couponCode.js
+++ js/app/directives/couponCode.js
@@ -1,4 +1,15 @@
 function couponCode() {
+	return {
+		restrict: 'A',
+		require:'ngModel',
+		link: function (scope, element, attrs, ngModel) {
+			ngModel.$validators.coupon = function(value){
+				debugger;
+        return /\d{2}[a-z]{4}\d{2}/i.test(value);
+      };
+		}
+	}
+
 
 }
 
