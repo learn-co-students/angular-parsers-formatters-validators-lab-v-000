1859dca9b8906cbd6bff29df1cef8192e1b73951
mirandalily <mirandaraymond@me.com>
2016-09-07 16:34:28 -0700

    Done.

--- js/app/directives/couponCode.js
+++ js/app/directives/couponCode.js
@@ -1,5 +1,14 @@
 function couponCode() {
-
+	return {
+		restrict: 'A',
+		require: 'ngModel',
+		link: function(scope, element, attrs, ngModel) {
+			ngModel.$validators.couponCode = function(value) {
+				var regex = (/\d{2}[a-z]{4}\d{2}/i);
+				return regex.test(value);
+			};
+		}
+	}
 }
 
 angular
