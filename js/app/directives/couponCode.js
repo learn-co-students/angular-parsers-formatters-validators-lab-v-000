edd53198d03970e1c06efaf22d189a6834cee1b1
jd2rogers2 <jd2rogers2@gmail.com>
2016-11-07 14:07:06 -0500

    question

--- js/app/directives/couponCode.js
+++ js/app/directives/couponCode.js
@@ -1,5 +1,14 @@
 function couponCode() {
-
+  return {
+    require: "ngModel",
+    restrict: 'A',
+    link: function(scope, elem, attrs, ngModel){
+      ngModel.$validators.couponCode = function(str){
+        return str.test(/\d{2}[a-z]{4}\d{2}/i);
+      };
+    };
+    // /\d{2}[a-z]{4}\d{2}/i
+  }
 }
 
 angular
--- js/app/directives/makeLower.js
+++ js/app/directives/makeLower.js
@@ -1,5 +1,17 @@
 function makeLower() {
-	
+  return {
+    require: 'ngModel',
+    restrict: 'A',
+    link: function(scope, elem, attrs, ngModel){
+      ngModel.$parsers.push(function(str){
+        return str.toLowerCase();
+      });
+
+      // ngModel.$formatters.push(function(str){
+      //   return str.toUpperCase();
+      // });
+    };
+  }
 }
 
 angular
