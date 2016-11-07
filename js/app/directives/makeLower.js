b24f0b03fa19f17818b2461f7d8d450a656b9615
Jesse Novotny <jessenovotny@gmail.com>
2016-11-07 14:29:04 -0700

    Done.

--- js/app/directives/makeLower.js
+++ js/app/directives/makeLower.js
@@ -1,5 +1,16 @@
 function makeLower() {
-	
+  return {
+    restrict: 'A',
+    require: 'ngModel',
+    link: function(scope, elem, attrs, ngModel){
+      ngModel.$parsers.push(function(coupon){
+        return coupon.toLowerCase();
+      });
+      // ngModel.$formatters.push(function(coupon){
+      //   return coupon.toLowerCase();
+      // })
+    }
+  }	
 }
 
 angular
