88035bc11f4d19b5a206eb5b7f9d15aac6658f1d
kade042 <dereje88@gmail.com>
2016-11-23 18:58:07 -0700

    Done.

--- js/app/directives/couponCode.js
+++ js/app/directives/couponCode.js
@@ -1,5 +1,13 @@
 function couponCode() {
-
+	return {
+			restrict: 'A',
+			require: 'ngModel',
+			link: function (scope, elem, atters, ngModel ) {
+				ngModel.$validators.coupon = function (value) {
+					return  value.length >= 4 ;
+				}
+			}
+	};
 }
 
 angular
