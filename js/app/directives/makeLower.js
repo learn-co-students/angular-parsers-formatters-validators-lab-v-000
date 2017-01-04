7b92091e47770b034fa8f06fd8efe6995243778a
Caleb Faught <faughtcj@gmail.com>
2017-01-04 13:55:49 -0600

    Done.

--- js/app/directives/makeLower.js
+++ js/app/directives/makeLower.js
@@ -1,5 +1,17 @@
 function makeLower() {
-	
+	return {
+		restrict: 'A',
+		require: 'ngModel',
+		link: function (scope, element, attrs, ngModel) {
+			ngModel.$formatters.push(function(text) {
+				return text.toLowerCase();
+			});
+
+			ngModel.$parsers.push(function(text) {
+				return text.toLowerCase();
+			});
+		}
+	}
 }
 
 angular
