7f666bb3d9943494e134d6aa89a0f71fbfd29b5f
tpstar <hanlee12@gmail.com>
2017-01-12 22:05:16 -0500

    done

--- js/app/directives/couponCode.js
+++ js/app/directives/couponCode.js
@@ -1,5 +1,14 @@
 function couponCode() {
-
+		var CODE_REGEXP = /\d{2}[a-z]{4}\d{2}/i;
+    return {
+        restrict: 'A',
+        require: 'ngModel',
+        link: function (scope, element, attrs, ctrl) {
+            ctrl.$validators.couponCode = function (value) {
+				return CODE_REGEXP.test(value);
+			}
+		}
+	}
 }
 
 angular
