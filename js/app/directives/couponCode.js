e57ea9af4f48289cec5c85c4ab90c19f8601796f
mcglaser <michaelcharlesglaser@gmail.com>
2017-04-06 14:53:36 -0700

    Done.

--- js/app/directives/couponCode.js
+++ js/app/directives/couponCode.js
@@ -1,4 +1,14 @@
 function couponCode() {
+  return {
+    restrict: 'A',
+    require: 'ngModel',
+    link: function(scope, element, attrs, ngModel) {
+      ngModel.$validators.coupon = function(value) {
+        var regex = /\d{2}[a-z]{4}\d{2}/i;
+        return regex.test(value);
+      };
+    }
+  };
 
 }
 
